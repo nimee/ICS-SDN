0c3ba2b23c68a4b6c70d095d410ed7af57ed7cdd
lsalab <lsalab@gmail.com>
2018-08-06 17:03:50 -0500

    some updates to the webui

--- nids/sdnidswebui.py
+++ nids/sdnidswebui.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import json
+import netaddr
 import cherrypy
 import pyonos
 
 class MainApp():
 
-    def __init__(self):
+    def __init__(self, idsconfig: str):
+        iconf = open(idsconfig, 'r').read()
+        self.__i_path = idsconfig
+        self.__i_conf = json.loads(iconf)
         self.__onos = pyonos.ONOSClient('192.168.56.50', 8181, 'onos', 'rocks')
 
     @cherrypy.expose
@@ -15,9 +20,79 @@ class MainApp():
     
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def getdevices(self):
+    def getdevices(self, *args, **kwargs):
         devices = self.__onos.devices()
+        if 'devroles' not in self.__i_conf:
+            self.__i_conf['devroles'] = {}
+        for i in range(len(devices)):
+            dev = devices[i]
+            if dev['id'] in self.__i_conf['devroles']:
+                dev['role'] = self.__i_conf['devroles'][dev['id']]
+            else:
+                dev['role'] = 'na'
+            devices[i] = dev
         return {'devs': devices}
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def gethosts(self, *args, **kwargs):
+        hosts = self.__onos.hosts()
+        if 'IP' not in self.__i_conf.keys():
+            self.__i_conf['IP'] = {}
+        for i in range(len(hosts)):
+            hst = hosts[i]
+            if hst['ipAddresses'][0] in self.__i_conf['IP'].values():
+                for ip in self.__i_conf['IP']:
+                    if self.__i_conf['IP'][ip] == hst['ipAddresses'][0]:
+                        hst['name'] = ip
+                        break
+            else:
+                hst['name'] = '*** UNKNOWN ***'
+            mac = netaddr.EUI(hst['mac'])
+            try:
+                oui = mac.oui
+                oui = oui.registration()
+                hst['mac'] += ' ({0:s})'.format(oui['org'])
+            except netaddr.core.NotRegisteredError:
+                hst['mac'] += ' (UNKNOWN)'
+            hosts[i] = hst
+        return {'hosts': hosts}
+    
+    @cherrypy.expose
+    def updatehostname(self, *args, **kwargs):
+        if any(kw not in kwargs.keys() for kw in ['ip', 'name']):
+            raise cherrypy.HTTPError(status=400, message='Missing parameters')
+        else:
+            ip = kwargs.pop('ip')
+            name = kwargs.pop('name')
+            pname = None
+            if 'IP' not in self.__i_conf.keys():
+                self.__i_conf['IP'] = {}
+            for i in self.__i_conf['IP']:
+                if self.__i_conf['IP'][i] == ip:
+                    pname = i
+                    break
+            if pname is not None:
+                self.__i_conf['IP'].pop(pname)
+            self.__i_conf['IP'][name] = ip
+            json.dump(self.__i_conf, open(self.__i_path, 'w'), indent='  ')
+            return 'OK'
+    
+    @cherrypy.expose
+    def updatedevrole(self, *args, **kwargs):
+        if any(kw not in kwargs.keys() for kw in ['id', 'role']):
+            raise cherrypy.HTTPError(status=400, message='Missing parameters')
+        else:
+            dev_id = kwargs.pop('id')
+            role = kwargs.pop('role')
+            if role not in ['bridge', 'field', 'honeypot']:
+                raise cherrypy.HTTPError(status=400, message='Invalid role')
+            else:
+                if 'devroles' not in self.__i_conf:
+                    self.__i_conf['devroles'] = {}
+                self.__i_conf['devroles'][dev_id] = role
+                json.dump(self.__i_conf, open(self.__i_path, 'w'), indent='  ')
+                return 'OK'
 
 if __name__ == '__main__':
     conf = {
@@ -44,6 +119,6 @@ if __name__ == '__main__':
         'log.error_file': 'webui/logs/error.log',
     }
     cherrypy.config.update(global_conf)
-    cherrypy.tree.mount(MainApp(), '/', conf)
+    cherrypy.tree.mount(MainApp('sdnconfig.json'), '/', conf)
     cherrypy.engine.start()
     cherrypy.engine.block()
--- nids/webui/static/js/webui.js
+++ nids/webui/static/js/webui.js
@@ -1,16 +1,37 @@
+
+function getSelecetdDeviceRole(id, role) {
+    var ret = "<select class=\"form-control\" id=\"drole-" + id + "\">";
+    switch(role) {
+        case "bridge":
+            ret += "<option value=\"bridge\" selected>Bridge</option><option value=\"field\">Field network</option><option value=\"honeypot\">Honeypot network</option><option value=\"na\">Unassigned</option>";
+            break;
+        case "field":
+            ret += "<option value=\"bridge\">Bridge</option><option value=\"field\" selected>Field network</option><option value=\"honeypot\">Honeypot network</option><option value=\"na\">Unassigned</option>";
+            break;
+        case "honeypot":
+            ret += "<option value=\"bridge\">Bridge</option><option value=\"field\">Field network</option><option value=\"honeypot\" selected>Honeypot network</option><option value=\"na\">Unassigned</option>";
+            break;
+        default:
+            ret += "<option value=\"bridge\">Bridge</option><option value=\"field\">Field network</option><option value=\"honeypot\">Honeypot network</option><option value=\"na\" selected>Unassigned</option>";
+    }
+    ret += "</select>"
+    return ret;
+}
+
 function refreshDevices() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             tbldata = JSON.parse(this.responseText);
-            var tblcontent = "<tr>";
+            var tblcontent = "<thead class=\"thead-light\">\r\n<tr>";
             tblcontent += "<th>Device ID</th>";
             tblcontent += "<th>Available</th>";
             tblcontent += "<th>Last update</th>";
             tblcontent += "<th>Manufacturer</th>";
             tblcontent += "<th>Hardware</th>";
             tblcontent += "<th>Protocol</th>";
-            tblcontent += "</tr>\r\n";
+            tblcontent += "<th>Configured role</th>";
+            tblcontent += "</tr>\r\n</thead>\r\n<tbody>\r\n";
             for (d in tbldata.devs) {
                 currdev = tbldata.devs[d];
                 annot = currdev.annotations;
@@ -24,8 +45,10 @@ function refreshDevices() {
                 tblcontent += "<td>" + currdev.mfr + "</td>";
                 tblcontent += "<td>" + currdev.hw + "</td>";
                 tblcontent += "<td>" + annot.protocol + "</td>";
+                tblcontent += "<td><div class=\"input-group input-group-sm\">" + getSelecetdDeviceRole(d, currdev.role) + "<div class=\"input-group-append\"><button type=\"button\" class=\"btn btn-sm btn-outline-success\" onclick=\"updateDeviceRole('" + currdev.id + "', document.getElementById('drole-" + d + "').value);\">Update</button></div></div></td>";
                 tblcontent += "</tr>\r\n";
             }
+            tblcontent += "</tbody>\r\n";
             tblelem = document.getElementById('devices');
             tblelem.innerHTML = tblcontent;
         }
@@ -34,22 +57,82 @@ function refreshDevices() {
     xhttp.send();
 }
 
+function refreshHosts() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            tbldata = JSON.parse(this.responseText);
+            var tblcontent = "<thead class=\"thead-light\">\r\n<tr>";
+            tblcontent += "<th>MAC address</th>";
+            tblcontent += "<th>IP addresses</th>";
+            tblcontent += "<th>Configured name</th>";
+            tblcontent += "<th>Locations</th>";
+            tblcontent += "</tr>\r\n</thead>\r\n<tbody>\r\n";
+            for (h in tbldata.hosts) {
+                currhost = tbldata.hosts[h];
+                tblcontent += "<tr>";
+                tblcontent += "<td>" + currhost.mac + "</td>";
+                tblcontent += "<td>" + currhost.ipAddresses.join(', ') + "</td>";
+                tblcontent += "<td><div class=\"input-group input-group-sm\"><input id=\"hname-" + h + "\" type=\"text\" class=\"form-control\" value=\"" + currhost.name + "\"><div class=\"input-group-append\"><button type=\"button\" class=\"btn btn-sm btn-outline-success\" onclick=\"updateHostName('" + currhost.ipAddresses[0] + "', document.getElementById('hname-" + h + "').value);\">Update</button></div></div></td>";
+                tblcontent += "<td>"
+                for (l in currhost.locations) {
+                    cloc = currhost.locations[l];
+                    cloc = cloc.elementId + " port: " + cloc.port + ",";
+                    cloc = cloc.substr(0, cloc.length - 1);
+                    tblcontent += cloc;
+                }
+                tblcontent += "</td>";
+                tblcontent += "</tr>\r\n";
+            }
+            tblcontent += "</tbody>\r\n";
+            tblelem = document.getElementById('hosts')
+            tblelem.innerHTML = tblcontent
+        }
+    }
+    xhttp.open('GET', '/gethosts', true);
+    xhttp.send();
+}
+
 function refreshAll() {
     localTime = document.getElementById('localtime');
     ctime = new Date();
     localTime.innerHTML = ctime.toLocaleDateString() + " " + ctime.toLocaleTimeString();
     refreshDevices();
+    refreshHosts();
 }
 
+function updateHostName(ip, name) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '/updatehostname', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("ip=" + ip + "&name=" + name);
+    refreshHosts();
+}
+
+function updateDeviceRole(id, role) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '/updatedevrole', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("id=" + id + "&role=" + role);
+    refreshDevices();
+}
+
+// Timer
 var timerId = -1;
 
 function setAutoRefresh(timeout) {
     if (timerId > 0)
         clearInterval(timerId);
     timerId = setInterval(refreshAll, timeout);
+    bt = document.getElementById('btn-timer');
+    bt.setAttribute('onclick', 'stopAutoRefresh();');
+    bt.innerHTML = "Stop auto-refresh";
 }
 
 function stopAutoRefresh() {
     if(timerId > 0)
         clearInterval(timerId);
+    bt = document.getElementById('btn-timer');
+    bt.setAttribute('onclick', 'setAutoRefresh(10000);');
+    bt.innerHTML = "Start auto-refresh";
 }
